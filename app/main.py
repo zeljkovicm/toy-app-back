ee238c33640f00240783fa349afa4f4f787385fd
zeljkovicm <zeljkovic_milan@yahoo.com>
2026-02-06 09:33:05 +0100

    Added OTEL for tracing

--- app/main.py
+++ app/main.py
@@ -27,6 +27,13 @@ from app.exceptions.exception_handlers import (
     review_not_allowed_error_handler
 )
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+
 app = FastAPI(title="Toy Store API", version="1.0.0")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
@@ -59,3 +66,14 @@ app.add_exception_handler(OrderNotFound, order_not_found_error_handler)
 app.add_exception_handler(ReviewAlreadyExistError, review_exist_error_handler)
 app.add_exception_handler(ReviewNotAllowedError,
                           review_not_allowed_error_handler)
+
+trace.set_tracer_provider(TracerProvider())
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://localhost:4318/v1/traces"
+)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+FastAPIInstrumentor.instrument_app(app)
